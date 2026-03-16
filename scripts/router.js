9e34d0ab69e40d92bccb7aabfa20c861830facd7
SouzaEdgar <edgarss2013@gmail.com>
2026-03-16 18:26:12 -0300

    fix: corrige roteamento do SPA em subpasta para o GitHub Pages

--- scripts/router.js
+++ scripts/router.js
@@ -1,20 +1,19 @@
+const BASE_PATH = window.location.hostname === 'localhost' ? '' : '/AdOps_Helper';
 const routes = {
-    404: {html: "screens/urls.html", js:"/pages/urls.js"},
-    "/": {html: "screens/urls.html", js:"/pages/urls.js"},
-    "/urls": {html: "screens/urls.html", js:"/pages/urls.js"},
-    "/tags": {html: "screens/tags.html", js:"/pages/tags.js"},
-    "/text": {html: "screens/text.html", js:"/pages/text.js"},
+    404: { html: "/screens/error.html", js: "./pages/urls.js" },
+    "/": { html: "/screens/urls.html", js: "./pages/urls.js" },
+    "/urls": { html: "/screens/urls.html", js: "./pages/urls.js" },
+    "/tags": { html: "/screens/tags.html", js: "./pages/tags.js" },
+    "/text": { html: "/screens/text.html", js: "./pages/text.js" },
 };
-
-console.log(window.location.hostname);
 
 async function navigate(path) {
     window.history.pushState({}, "", path);
 
-    const route = routes[path] || routes[404];
+    const route = routes[path.replace(BASE_PATH,"")] || routes[404];
 
     try {
-        const resp = await fetch(route.html);
+        const resp = await fetch(BASE_PATH + route.html);
         const html = await resp.text();
         document.getElementById("index").innerHTML = html;
 
@@ -36,16 +35,16 @@ window.pathRedirect = null;                                                 // l
 
 document.getElementById("link_urls").addEventListener("click", (event) => {
     event.preventDefault();
-    navigate('/urls');
+    navigate(BASE_PATH+'/urls');
 });
 
 document.getElementById("link_tags").addEventListener("click", (event) => {
     event.preventDefault();
-    navigate('/tags');
+    navigate(BASE_PATH+'/tags');
 });
 
 document.getElementById("link_text").addEventListener("click", (event) => {
     event.preventDefault();
-    navigate('/text');
+    navigate(BASE_PATH+'/text');
 });
 
