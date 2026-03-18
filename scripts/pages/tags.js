81d98993393894d19927744c90fcad99f3b8931b
SouzaEdgar <edgarss2013@gmail.com>
2026-03-17 21:02:18 -0300

    feat: comunicação via postMessage entre tags e preview

--- scripts/pages/tags.js
+++ scripts/pages/tags.js
@@ -3,24 +3,29 @@ export function init() {
     const btnVerificar = document.getElementById("btnVerificarTag");
     const preview = document.getElementById("preview");
 
-
     btnVerificar.addEventListener("click", () => {
         const tagCodigo = txtArea.value;
         if (!tagCodigo) return alert("Cole uma tag primeiro!");
 
         preview.innerHTML = "";                                     // --- limpar o container para uma nova tag
-        localStorage.setItem('tag_preview', tagCodigo);             // --- salvar as tags
 
-        // --- Criar o iframe "avo" (container para isolamento) --- //
         const iframe = document.createElement("iframe");
-        iframe.className = "iframeTag"
+        iframe.className = "iframeTag";
         iframe.style.width = "100%";
-        iframe.style.minHeight = "250px";
+        iframe.style.minHeight = "600px";
         iframe.style.border = "1px dashed #ccc";
         iframe.setAttribute('sandbox', 'allow-scripts allow-popups allow-popups-to-escape-sandbox');
-        //iframe.setAttribute('sandbox', 'allow-scripts allow-popups allow-same-origin');               // para teste
 
-        iframe.src = "preview.html";
+        // --- verifica se está rodando local ou não para montar o caminho correto --- //
+        const isLocal = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1";
+        const basePath = isLocal ? "" : "/AdOps_Helper";
+
+        iframe.src = `${basePath}/screens/preview.html`;            // --- dessa vez aponta para a pasta correta
+
+        // enviar a tag para o preview
+        iframe.onload = () => {
+            iframe.contentWindow.postMessage(tagCodigo, "*");
+        };
 
         preview.appendChild(iframe);
     });
